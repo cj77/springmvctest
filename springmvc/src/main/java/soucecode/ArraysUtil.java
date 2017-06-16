e0d9a4c9740ab7749dbebf9010b58f6074248399
chenj <cjxy2k@126.com>
2017-06-16 09:32:42 +0800

    test

---
+++ springmvc/src/main/java/soucecode/ArraysUtil.java
@@ -0,0 +1,24 @@
+package soucecode;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class ArraysUtil {
+	@Test
+	public void testCopy() {
+		int value[] = { 1, 3, 5, 2 };
+		int cvalue[] = Arrays.copyOf(value, value.length);
+		System.out.println(Arrays.toString(cvalue));
+
+		int index = Arrays.binarySearch(cvalue, 5);
+		System.out.println(index);
+
+		Arrays.sort(cvalue);
+		System.out.println(Arrays.toString(cvalue));
+		
+		String svalue[] = { "10b", "101a", "2c", "z", "m" };
+		Arrays.sort(svalue);
+		System.out.println(Arrays.toString(svalue));
+	}
+}
